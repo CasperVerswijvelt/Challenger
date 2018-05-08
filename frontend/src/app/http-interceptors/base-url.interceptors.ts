bdecdaeb5f582a092e0e7fc1d418ed5b0d08f110
Casper Verswijvelt <casper.verswijvelt.y7500@student.hogent.be>
2018-05-08 17:16:07 +0200

    build_files

--- frontend/src/app/http-interceptors/base-url.interceptors.ts
+++ frontend/src/app/http-interceptors/base-url.interceptors.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { BACKEND_URL } from '../../environments/environment';
+import { BACKEND_URL } from '../../environments/environment.prod';
+
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
